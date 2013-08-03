7695d7c2bff47058d997befa95ea27de5dcb1fda
villanopack <villa@dvillanueva.com>
2013-08-03 12:47:42 +0200

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EmberDeviseRails::Application.config.secret_token = '24ddf09a217d04bd1ca93956e2aabe73e718c3671272433dbf0d550e52ce3c05c12b65d923572ca7f70af6a77fd2a337488ee50f32d7e71bc62d830aff91971b'
